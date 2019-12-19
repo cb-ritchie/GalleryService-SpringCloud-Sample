8989d2f91ebe6e9ed1aabb5eb0ac434275170a7f
cb-Ritchie <ritchie@chargebee.com>
2019-12-19 11:55:13 +0530

    Sample Gallery Service Completed

---
+++ src/main/java/com/chargebee/gw/demo/galleryservice/model/Gallery.java
@@ -0,0 +1,25 @@
+package com.chargebee.gw.demo.galleryservice.model;
+
+import java.util.List;
+
+public class Gallery {
+
+    private int id;
+    private List<Object> images;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Object> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Object> images) {
+        this.images = images;
+    }
+}
---
+++ src/test/java/com/chargebee/gw/demo/galleryservice/GalleryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chargebee.gw.demo.galleryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
